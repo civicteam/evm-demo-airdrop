1eb1edac7eea90129c5463e64dc2c3389659709f
dankelleher <dankelleher@yahoo.com>
2024-02-09 14:53:14 +0100

    Wagmi improvements

--- app/src/AirdropContext.tsx
+++ app/src/AirdropContext.tsx
@@ -1,43 +1,58 @@
-import React, { createContext, FC, ReactNode, useContext, useEffect, useState } from "react";
-import {Airdrop, Airdrop__factory} from "./typechain-types";
+import React, { createContext, FC, ReactNode, useContext } from "react";
+import {Airdrop__factory} from "./typechain-types";
 import {useWallet} from "./useWallet";
+import {useReadContract, useWriteContract} from "wagmi";
+import type {Address} from "abitype";
+import {UseWriteContractReturnType} from "wagmi/src/hooks/useWriteContract.ts";
 
-const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS || "";
+const contractAddress:Address  = import.meta.env.VITE_CONTRACT_ADDRESS as Address || "0xabc";
 
 type AirdropContextType = {
-  client: Airdrop | undefined;
-  hasToken: boolean;
-  totalTokens: bigint;
+  balance: bigint | undefined;
+  totalSupply: bigint | undefined;
+  isPending: boolean;
+  txHash: string | undefined;
+  error: UseWriteContractReturnType["error"] | null;
+  claim: (...args: any[]) => void;
 }
 export const AirdropContext = createContext<AirdropContextType>({
-  client: undefined,
-  hasToken: false,
-  totalTokens: 0n,
+  balance: undefined,
+  totalSupply: undefined,
+  isPending: false,
+  txHash: undefined,
+  error: null,
+  claim: () => {}
 });
 
 export const AirdropProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const wallet = useWallet();
-  const [client, setClient] = useState<Airdrop | undefined>();
-  const [ hasToken, setHasToken ] = useState(false);
-  const [ totalTokens, setTotalTokens ] = useState<bigint>(0n);
 
-  useEffect(() => {
-    if (!wallet?.signer || !wallet?.address) return undefined;
-    const airdrop = Airdrop__factory.connect(
-        contractAddress,
-        wallet.signer
-    );
-    setClient(airdrop)
-    airdrop.totalSupply().then((total) => setTotalTokens(total));
-    airdrop.balanceOf(wallet.address).then((balance) => {
-      if (balance > 0) {
-        setHasToken(true);
-      }
+  const {
+    data: balance
+  } = useReadContract({
+    address: contractAddress,
+    abi: Airdrop__factory.abi,
+    functionName: "balanceOf",
+    args: [wallet?.address as Address]
+  });
+
+  const { data: totalSupply } = useReadContract({
+    address: contractAddress,
+    abi: Airdrop__factory.abi,
+    functionName: "totalSupply",
+  })
+
+  const { data: txHash, isPending, writeContract, error } = useWriteContract()
+
+  const claim = () => {
+    writeContract({
+      address: contractAddress,
+      abi: Airdrop__factory.abi,
+      functionName: "claim",
     });
-  }, [wallet?.address]);
-
+  }
   return (
-      <AirdropContext.Provider value={{ client, hasToken, totalTokens }}>
+      <AirdropContext.Provider value={{ balance, totalSupply, claim, isPending, txHash, error }}>
         {children}
       </AirdropContext.Provider>
   );
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css';
 import {useAirdrop} from "./AirdropContext";
-import {useAccount} from "wagmi";
+import {useAccount, usePrepareTransactionRequest, useSendTransaction} from "wagmi";
 import {ConnectButton} from '@rainbow-me/rainbowkit';
 import {GatewayStatus, IdentityButton, useGateway} from "@civic/ethereum-gateway-react";
 
 const Dashboard = () => {
-    const { isConnected } = useAccount()
-    const { client, hasToken } = useAirdrop();
+    const { balance, totalSupply, claim, isPending, txHash, error } = useAirdrop();
     const { gatewayStatus } = useGateway();
-    if (!client || !isConnected) return <></>;
 
     return (<div style={{
         display: "flex",
@@ -19,9 +17,13 @@ const Dashboard = () => {
         <h1>Claim your Airdrop</h1>
         <IdentityButton />
         { gatewayStatus !== GatewayStatus.ACTIVE && <div>Verify you are a unique person before entering</div>}
-        { !hasToken && <button disabled={gatewayStatus !== GatewayStatus.ACTIVE} onClick={() => client.claim()}>{ gatewayStatus !== GatewayStatus.ACTIVE ? "Verify first!": "Claim"}</button>}
-        { (hasToken || (gatewayStatus !== GatewayStatus.ACTIVE) && <button onClick={() => client.claim({gasLimit: 3000000})}>I don't care, try to claim anyway!</button>)}
-        { hasToken && <p><>You have a token</></p>}
+        { !balance && <button disabled={gatewayStatus !== GatewayStatus.ACTIVE} onClick={claim}>{ gatewayStatus !== GatewayStatus.ACTIVE ? "Verify first!": "Claim"}</button>}
+        { (!!balance || (gatewayStatus !== GatewayStatus.ACTIVE) )&& <button onClick={() => claim({gasLimit: 3000000})}>I don't care, try to claim anyway!</button>}
+        { isPending && <p><>Claiming...</></p>}
+        { !!balance && <p><>Congratulations, you have a token!</></p>}
+        { txHash && <p><>Transaction: {txHash}</></p>}
+        { error && <p><>Error: {error.message}</></p>}
+        <div>Claimed so far: { totalSupply?.toString() ?? 0 }</div>
     </div>)
 }
 function App() {
