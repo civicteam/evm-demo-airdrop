b8705d461ff8ca3b460d938b83ea0c655880b7d2
dankelleher <dankelleher@yahoo.com>
2024-02-13 08:10:50 +0100

    Styling

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,31 +1,34 @@
 import './App.css';
-import {useAirdrop} from "./AirdropContext";
-import {useAccount, usePrepareTransactionRequest, useSendTransaction} from "wagmi";
+import {useAirdrop} from './AirdropContext';
 import {ConnectButton} from '@rainbow-me/rainbowkit';
-import {GatewayStatus, IdentityButton, useGateway} from "@civic/ethereum-gateway-react";
+import {GatewayStatus, IdentityButton, useGateway} from '@civic/ethereum-gateway-react';
+import Ticket from "./ticket.svg?react";
 
 const Dashboard = () => {
-    const { balance, totalSupply, claim, isConfirming, isConfirmed, txHash, error } = useAirdrop();
+    const { balance, totalSupply, claim, isConfirming, txHash, error } = useAirdrop();
     const { gatewayStatus } = useGateway();
 
-    return (<div style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: "0.5rem"
-    }}>
-        <h1>Claim your Airdrop</h1>
-        <IdentityButton />
-        { gatewayStatus !== GatewayStatus.ACTIVE && <div>Verify you are a unique person before entering</div>}
-        { !balance && <button disabled={gatewayStatus !== GatewayStatus.ACTIVE} onClick={claim}>{ gatewayStatus !== GatewayStatus.ACTIVE ? "Verify first!": "Claim"}</button>}
-        { (!!balance || (gatewayStatus !== GatewayStatus.ACTIVE) )&& <button onClick={() => claim({gasLimit: 3000000})}>I don't care, try to claim anyway!</button>}
-        { isConfirming && <p><>Claiming...</></p>}
-        { !!balance && <p><>Congratulations, you have a token!</></p>}
-        { txHash && <p><>Transaction: {txHash}</></p>}
-        { error && <p><>Error: {error.message}</></p>}
-        <div>Claimed so far: { totalSupply?.toString() ?? 0 }</div>
+    return (<div id="main">
+        <Ticket />
+        <h1>Claim Airdrop</h1>
+        <IdentityButton/>
+        {gatewayStatus !== GatewayStatus.ACTIVE && <div>Verify you are a unique person before entering</div>}
+        {!balance && <button disabled={gatewayStatus !== GatewayStatus.ACTIVE}
+                             onClick={claim}>{gatewayStatus !== GatewayStatus.ACTIVE ? "Verify first!" : "Claim"}</button>}
+        {(!!balance || (gatewayStatus !== GatewayStatus.ACTIVE)) &&
+            <button onClick={() => claim({gasLimit: 3000000})}>I don't care, try to claim anyway!</button>}
+        {isConfirming && <p><>Claiming...</>
+        </p>}
+        {!!balance && <p><>Congratulations, you have a token!</>
+        </p>}
+        {txHash && <p><>Transaction: {txHash}</>
+        </p>}
+        {error && <p><>Error: {error.message}</>
+        </p>}
+        <div>Claimed so far: {totalSupply?.toString() ?? 0}</div>
     </div>)
 }
+
 function App() {
     return (
         <div className="App">
@@ -36,7 +39,7 @@ function App() {
                 justifyContent: "center",
                 height: "100vh"
             }}>
-                <ConnectButton/>
+                <ConnectButton showBalance={false}/>
                 <hr/>
                 <Dashboard/>
             </div>
