ab618c5e65503b0ab5029dd680e418bec22a125c
dankelleher <dankelleher@yahoo.com>
2024-02-14 14:59:39 +0100

    Contract comments

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -2,21 +2,23 @@
 pragma solidity ^0.8.20;
 
 import "./IERC20.sol";
+/* Step 1: Import this (hardhat version) */
 import "@identity.com/gateway-protocol-eth/contracts/Gated.sol";
 
+/* Step 2: Add the Gated trait */
 contract Airdrop is IERC20, Gated {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "Solidity by Example";
-    string public symbol = "SOLBYEX";
+    string public name = "My simple ERC20";
+    string public symbol = "ERC20";
     uint8 public decimals = 18;
 
-    /* Step 1: Add a constructor */
+    /* Step 3: Add a constructor */
     constructor(address gatewayTokenContract, uint256 gatekeeperNetwork) Gated(gatewayTokenContract, gatekeeperNetwork){
     }
 
-    /* Step 2: Gate your function */
+    /* Step 4: Gate your function */
     function claim() external gated /* This is where the magic happens */ {
         require(balanceOf[msg.sender] == 0, "You already claimed your tokens");
         balanceOf[msg.sender] += 1;
